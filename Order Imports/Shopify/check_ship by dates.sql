29cb9cb6d279a2beaf096a50bc328f6a667ba18d
Jillian Kearns <j.birum1@gmail.com>
2026-04-27 12:39:02 -0400

    Update check_ship by dates.sql

--- Order Imports/Shopify/check_ship by dates.sql
+++ Order Imports/Shopify/check_ship by dates.sql
@@ -2,8 +2,8 @@ select
 b.order_number,
 a.ship_by_dtm as ship_by_date_gift,
 b.ship_by_date as ship_by_date_order
-from titan.testing.dbo.import_order_gift a j
-oin titan.testing.dbo.orders b 
+from titan.testing.dbo.import_order_gift a
+join titan.testing.dbo.orders b 
 on a.third_party_order_number = b.import_order_number 
 where b.order_number like 'hwebs142%'
 
