77ea47c498fc3d82b6556fb9063739ae64b2f4f9
Jillian Kearns <j.birum1@gmail.com>
2026-04-27 12:54:54 -0400

    Update check_ship by dates.sql

--- Order Imports/Shopify/check_ship by dates.sql
+++ Order Imports/Shopify/check_ship by dates.sql
@@ -1,7 +1,7 @@
 select 
 b.order_number,
 a.ship_by_dtm as ship_by_date_gift,
-b.ship_by_date as ship_by_date_order
+b.expected_ship_date as ship_by_date_order
 from titan.testing.dbo.import_order_gift a
 join titan.testing.dbo.orders b 
 on a.third_party_order_number = b.import_order_number 
